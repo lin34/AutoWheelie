2335d7f5a5d57a86f93101086ac7e42970a0273b
Michael Lin <michael.jlin7@gmail.com>
2022-02-22 19:42:02 -0500

    updated firmware with lin act and pwm motor output

--- fydp_firmware/fydp_firmware/fydp_firmware.ino
+++ fydp_firmware/fydp_firmware/fydp_firmware.ino
@@ -1,8 +1,5 @@
 #include <Wire.h>
-#include <Adafruit_MCP4725.h>
 
-Adafruit_MCP4725 dac1;
-Adafruit_MCP4725 dac2;
 #define BUTTON_UP 2
 #define BUTTON_RIGHT 3
 #define BUTTON_DOWN 4
@@ -13,8 +10,10 @@ Adafruit_MCP4725 dac2;
 #define PIN_ANALOG_X 0
 #define PIN_ANALOG_Y 1
 
-#define LINOUT10 10
+#define MOTOR_9 9
+#define MOTOR_10 10
 #define LINOUT11 11
+#define LINOUT12 12
 
 #define DELAY 100
 const uint8_t   OFF     = HIGH;
@@ -46,23 +45,13 @@ void setup() {
   pinMode(BUTTON_K, INPUT);
  digitalWrite(BUTTON_K, HIGH);
 
- pinMode(LINOUT10, OUTPUT);
 
+pinMode(MOTOR_9, OUTPUT);
+pinMode(MOTOR_10, OUTPUT);
 
-  pinMode(LINOUT11, OUTPUT);
+ pinMode(LINOUT11, OUTPUT);
+ pinMode(LINOUT12, OUTPUT);
  
-
-   // For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
-  // For MCP4725A0 the address is 0x60 or 0x61
-  // For MCP4725A2 the address is 0x64 or 0x65
-  bool found1 = dac1.begin(0x62);
-  bool found2 = dac2.begin(0x63);
-  if(found1){
-    Serial.println("Found dac 1");
-  }
-  if(found2){
-    Serial.println("Found dac 2");
-  }
 
 }
 //Reverses a buttons reading, to make it more readable
@@ -70,21 +59,11 @@ int rBtn(int btn){
 return (btn==0)?1:0;
 }
 
-//Maps the joystick reading to an analog out between 0 to 4095
-//on the DAC X mid = 505 Y mid = 509
-//the motor only goes from 1.2v to 4.2v, so the ranges should be restricted from 983 to 3439
-int motorControlX(int val){
-  if(val<506) return 0;//only move when moving joystick up
-  val = val - 506;//1023 - 509 = 514
-  //map 0 - 506
-  return map(val, 0, 514,983,3439);  
-
-}
 int motorControlY(int val){
   if(val<509) return 0;//only move when moving joystick up
   val = val - 509;//1023 - 509 = 514
   //map 0 - 509 to 0 - 4095
-  return map(val, 0, 514,983,3439);  
+  return map(val, 0, 514,0,255);  
 
 }
 
@@ -113,26 +92,29 @@ void loop() {
   //dac ranges from 0 to 4095
   //x axis for right motor
   //Read joystick value y axis for left motor
-  int motorValX = motorControlX(joyXVal);
+
   int motorValY = motorControlY(joyYVal);
+  digitalWrite(MOTOR_9,motorValY);
+  digitalWrite(MOTOR_10,motorValY);
 
   //send speed control analog signal to motor controller
-  dac1.setVoltage(motorValX, false);
-  dac2.setVoltage(motorValY, false);//vout connected to A0
+
+
+  
   //Extend linear actuator
   if(btnLinActUp==0){
-   digitalWrite(LINOUT10, HIGH);
-    digitalWrite(LINOUT11, LOW);
+   digitalWrite(LINOUT11, HIGH);
+    digitalWrite(LINOUT12, LOW);
   }
   //Retract linear actuator
   if(btnLinActDown==0){
-   digitalWrite(LINOUT10, LOW);
-    digitalWrite(LINOUT11, HIGH);
+   digitalWrite(LINOUT11, LOW);
+    digitalWrite(LINOUT12, HIGH);
   }
   //stop linear actuator
   if(btnStopLin==0){
-   digitalWrite(LINOUT10, LOW);
-    digitalWrite(LINOUT11, LOW);
+   digitalWrite(LINOUT11, LOW);
+    digitalWrite(LINOUT12, LOW);
   }
 
   
